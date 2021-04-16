429b9ab156840697ddbb82a8bcff98b421ed9a00
Emad <m.akhavan.saleh@gmail.com>
2021-04-16 22:15:52 +0430

    Add WebAppChart Project to GitHub

---
+++ WebAppChart/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppChart.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Data = "15,30,47,8";
+            return View();
+        }
+    }
+}
---
+++ WebAppChart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppChart.MvcApplication" Language="C#" %>
